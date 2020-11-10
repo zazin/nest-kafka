7ba1d05701f152312a5e5a0c1b509d6089cb827f
nur zazin <nurza.cool@gmail.com>
2020-11-10 11:20:10 +0700

    add docker kafka

--- .debian_kafka_zoo/docker-entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-#/home/kafka/kafka/bin/zookeeper-server-start.sh /home/kafka/kafka/config/zookeeper.properties && \
-/home/kafka/kafka/bin/kafka-server-start.sh /home/kafka/kafka/config/server.properties
